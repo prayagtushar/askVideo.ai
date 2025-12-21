f97ce7a5449704ed7fe9f7fed031d1cc4028cda2
prayagtushar <prayagtushar2016@gmail.com>
2025-12-22 01:20:52 +0530

    Code Cleanup

--- packages/db/prisma/migrations/20251221182904_/migration.sql
+++ packages/db/prisma/migrations/20251221182904_/migration.sql
@@ -1,7 +1,17 @@
 -- CreateTable
+CREATE TABLE "Video" (
+    "id" TEXT NOT NULL,
+    "youtubeId" TEXT NOT NULL,
+    "title" TEXT,
+    "transcript" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Video_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "ChatSession" (
     "id" TEXT NOT NULL,
-    "videoUrl" TEXT NOT NULL,
     "videoId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -11,13 +21,19 @@ CREATE TABLE "ChatSession" (
 -- CreateTable
 CREATE TABLE "Message" (
     "id" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
     "role" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
     "chatSessionId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Video_youtubeId_key" ON "Video"("youtubeId");
+
+-- AddForeignKey
+ALTER TABLE "ChatSession" ADD CONSTRAINT "ChatSession_videoId_fkey" FOREIGN KEY ("videoId") REFERENCES "Video"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Message" ADD CONSTRAINT "Message_chatSessionId_fkey" FOREIGN KEY ("chatSessionId") REFERENCES "ChatSession"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
