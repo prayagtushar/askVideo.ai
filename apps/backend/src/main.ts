49c372952a65c1dcebd8128af68693feab496f25
prayagtushar <prayagtushar2016@gmail.com>
2025-12-22 01:21:16 +0530

    Checkpoint

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ChatModule } from './chat/chat.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(ChatModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
---
+++ apps/backend/src/types.ts
@@ -0,0 +1,5 @@
+export interface Chunks {
+    id : string,
+    values :  number[],
+    metadata : any;
+}
